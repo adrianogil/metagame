4b276faa62a3100c92a94d69ba78cbe0b9ad7ed8
Adriano Gil <adrianogil.san@gmail.com>
2020-08-02 17:02:28 -0400

    Removed obsolete module 'simpleenemies'

--- src/python/metagame/simpleenemies/__main__.py
+++
@@ -1,6 +0,0 @@
-from metagame.simpleenemies import MetaGameFramework
-
-if __name__ == "__main__":
-    print("Running 'simpleenemies' game")
-    game = MetaGameFramework.create_game()
-    game.play()
