cfe0053f6e532660aa87546942b89e9057c2ea5d
Adriano Gil <adrianogil.san@gmail.com>
2023-11-18 10:57:13 -0400

    feat: allow calc command to parse concepts

--- src/python/metagame/actions/calcaction.py
+++ src/python/metagame/actions/calcaction.py
@@ -1,53 +1,23 @@
-from metagame.utils.numbers import is_integer
+from math import *
+import re
 
 
 def run_calc_action(action_parser, data, parent_args):
+    # Step 1: Extract the calculation string
     calc_string = data[0]
 
-    current_number = [0, 0]
+    # Step 2: Identify variables
+    variables = re.findall(r'#(.*?)#', calc_string)
 
-    calc_data = {
-        "register": 0,
-        "inside_number": False,
-        "operation": None
-    }
+    # Step 3: Replace variables with their values
+    for var in variables:
+        print(var)
+        value = action_parser.get_concept(var)
+        print(value)
+        calc_string = calc_string.replace(f'#{var}#', str(value))
 
-    operations = ['+', '-', '*', '/']
+    # Step 4: Evaluate the calculation string
+    result = eval(calc_string)
 
-    def update_operation():
-        if calc_data["register"] == 1:
-            if calc_data["operation"] == "+":
-                current_number[0] = current_number[0] + current_number[1]
-            elif calc_data["operation"] == "-":
-                current_number[0] = current_number[0] - current_number[1]
-            elif calc_data["operation"] == "*":
-                current_number[0] = current_number[0] * current_number[1]
-            elif calc_data["operation"] == "/":
-                current_number[0] = current_number[0] / current_number[1]
-
-            calc_data["operation"] = None
-            calc_data["register"] = 0
-        else:
-            calc_data["register"] = 1
-        calc_data["inside_number"] = False
-
-    for s in calc_string:
-        if s == ' ':
-            calc_data["inside_number"] = False
-            update_operation()
-            continue
-
-        if is_integer(s):
-            if not calc_data["inside_number"]:
-                current_number[calc_data["register"]] = int(s)
-                calc_data["inside_number"] = True
-            else:
-                current_number[calc_data["register"]] = current_number[calc_data["register"]] * 10 + int(s)
-        else:
-            update_operation()
-
-            if s in operations:
-                calc_data["operation"] = s
-    update_operation()
-
-    return current_number[0]
+    return result
+    
